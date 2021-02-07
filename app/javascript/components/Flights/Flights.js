5b33e49f0596f1029c389af5c95087291dea41fc
Mikey Esteban <michaelp.esteban@gmail.com>
2021-02-07 10:36:24 -0500

    added passengers amount dropdown, refactoed useEffect and toggleSelected functions

--- app/javascript/components/Flights/Flights.js
+++ app/javascript/components/Flights/Flights.js
@@ -9,6 +9,13 @@ const FlightsWrapper = styled.div`
     background: #f2e9e4; /* soft pink */
   }
 `
+const SearchWrapper = styled.div`
+  display: flex;
+  justify-content: space-around;
+
+  max-width: 600px;
+  margin: 0 auto;
+`
 
 const Flights = () => {
 
@@ -16,11 +23,21 @@ const Flights = () => {
   const [ fromLocation, setFromLocation ] = useState([])
   const [ toLocation, setToLocation ] = useState([])
   const [ travelDates, setTravelDates ] = useState([])
+  const [ passengers, setPassengers ] = useState([])
 
   useEffect( () => {
+    // set Passengers dropdown data
+    const passengers = []
+    for (let i = 1; i < 5; i++) {
+      const data = { id: i, title: i, selected: false, key: 'passengers' }
+      passengers.push(data)
+    }
+    setPassengers(passengers)
+
     axios.get('/api/v1/flights')
       .then( resp => {
         setFlights(resp.data.data)
+        // set Dates dropdown data
         const datesArr = resp.data.data.map( item => item.attributes.start.split(' ')[1] )
         const uniqueDates = [...new Set(datesArr)]
         const datesData = uniqueDates.map( (date, index) => {
@@ -35,46 +52,37 @@ const Flights = () => {
       })
       .catch(resp => console.log(resp))
 
+    // set toLocation & fromLocation dropdown data
     axios.get('/api/v1/airports')
       .then ( resp => {
         const airports = resp.data.data
-        const toData = airports.map( airport => {
-          return ({
+        const keys = ['toLocation', 'fromLocation']
+        for (let i = 0; i < 2; i++) {
+          const data = airports.map( airport => {
+            return ({
               id: airport.id,
               title: airport.attributes.code,
               selected: false,
-              key: 'toLocation'
+              key: keys[i]
+            })
           })
-        })
-        const fromData = airports.map( airport => {
-          return ({
-              id: airport.id,
-              title: airport.attributes.code,
-              selected: false,
-              key: 'fromLocation'
-          })
-        })
-        setToLocation(toData)
-        setFromLocation(fromData)
+          i === 0 ? setToLocation(data) : setFromLocation(data) ;
+        }
       })
       .catch(resp => console.log(resp))
   }, [])
 
   const toggleSelected = (id, key) => {
-    console.log('IN TOGGLE SELECTED..', key);
-    if (key === 'toLocation') {
-      toLocation.forEach( item => {
-        item.id === id ? item.selected = true : item.selected = false ;
-      })
-    } else if (key === 'fromLocation') {
-      fromLocation.forEach( item => {
-        item.id === id ? item.selected = true : item.selected = false ;
-      })
-    } else {
-      travelDates.forEach( item => {
-        item.id === id ? item.selected = true : item.selected = false ;
-      })
+    const legend = {
+      'toLocation': toLocation,
+      'fromLocation': fromLocation,
+      'passengers': passengers,
+      'travelDates': travelDates
     }
+
+    legend[key].forEach( item => {
+      item.id === id ? item.selected = true : item.selected = false ;
+    })
   }
 
   const flightsList = flights.map( item => <Flight key={item.id} attributes={item.attributes} /> )
@@ -82,9 +90,20 @@ const Flights = () => {
   return (
     <Fragment>
       <div>[My Home Component]</div>
-      <Dropdown title='Select location' list={toLocation} toggleSelected={toggleSelected} />
-      <Dropdown title='Select location' list={fromLocation} toggleSelected={toggleSelected} />
-      <Dropdown title='Select date' list={travelDates} toggleSelected={toggleSelected} />
+      <SearchWrapper>
+        <DropdownWrapper>
+          <Dropdown title='Select location' list={toLocation} toggleSelected={toggleSelected} />
+        </DropdownWrapper>
+        <DropdownWrapper>
+          <Dropdown title='Select location' list={fromLocation} toggleSelected={toggleSelected} />
+        </DropdownWrapper>
+        <DropdownWrapper>
+          <Dropdown title='Select date' list={travelDates} toggleSelected={toggleSelected} />
+        </DropdownWrapper>
+        <DropdownWrapper>
+          <Dropdown title='Select passenger' list={passengers} toggleSelected={toggleSelected} />
+        </DropdownWrapper>
+      </SearchWrapper>
       <FlightsWrapper>
         {flightsList}
       </FlightsWrapper>
