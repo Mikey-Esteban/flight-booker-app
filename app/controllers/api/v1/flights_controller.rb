90c4bd46c5a5460d7698da78875aafcea4205bb4
Mikey Esteban <michaelp.esteban@gmail.com>
2021-02-08 10:39:43 -0500

    can select flights with departing, arriving, date search params

--- app/controllers/api/v1/flights_controller.rb
+++ app/controllers/api/v1/flights_controller.rb
@@ -5,17 +5,31 @@ class Api::V1::FlightsController < ApplicationController
     puts "---------"
     puts "params: #{params}"
     puts "---------"
-    if params.keys.include?('from')
-      puts "whats up"
-      puts "from: #{params['from']}"
-      puts "..... finding airport_id for #{params['from']}"
+
+    if params.keys.include?('from') && params.keys.include?('to')
+      origin_id = find_airport_id params['from']
+      destination_id = find_airport_id params['to']
+      flights = Flight.where('flights.origin_id = ?', origin_id)
+                      .where('flights.destination_id = ?', destination_id)
+                      .order(start: :asc)
+    elsif params.keys.include?('from')
       id = find_airport_id params['from']
-      puts "..... airport_id: #{id}"
       flights = Flight.where('flights.origin_id = ?', id).order(start: :asc)
+    elsif params.keys.include?('to')
+      id = find_airport_id params['to']
+      flights = Flight.where('flights.destination_id = ?', id).order(start: :asc)
     else
       flights = Flight.all.order(start: :asc)
     end
 
+    if params.keys.include?('date')
+      puts " HI YOOOOOOOO"
+      puts "flights so far.. #{flights.length}"
+      formatted_date = format_date(params['date'])
+      puts "date: #{formatted_date}"
+
+      flights = flights.where("DATE(start) = ?", formatted_date)
+    end
 
     render json: FlightSerializer.new(flights).serializable_hash.to_json
   end
@@ -26,6 +40,12 @@ class Api::V1::FlightsController < ApplicationController
   def find_airport_id code
     airport = Airport.find_by(code: code)
     airport.id
+  end
+
+  def format_date date
+    date_array = date.split('.')
+    formatted_date = "#{date_array[2]}-#{date_array[0]}-#{date_array[1]}"
+    formatted_date
   end
 
 end
