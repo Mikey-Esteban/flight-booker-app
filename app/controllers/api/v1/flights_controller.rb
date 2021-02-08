cd1553aacf1a9e219c5fe86f5d5c4aacfed183cb
Mikey Esteban <michaelp.esteban@gmail.com>
2021-02-08 10:47:27 -0500

    refactored flights#index

--- app/controllers/api/v1/flights_controller.rb
+++ app/controllers/api/v1/flights_controller.rb
@@ -2,32 +2,20 @@ class Api::V1::FlightsController < ApplicationController
   protect_from_forgery with: :null_session
 
   def index
-    puts "---------"
-    puts "params: #{params}"
-    puts "---------"
+    flights = Flight.all.order(start: :asc)
 
-    if params.keys.include?('from') && params.keys.include?('to')
+    if params.keys.include?('from')
       origin_id = find_airport_id params['from']
+      flights = flights.where('flights.origin_id = ?', origin_id)
+    end
+
+    if params.keys.include?('to')
       destination_id = find_airport_id params['to']
-      flights = Flight.where('flights.origin_id = ?', origin_id)
-                      .where('flights.destination_id = ?', destination_id)
-                      .order(start: :asc)
-    elsif params.keys.include?('from')
-      id = find_airport_id params['from']
-      flights = Flight.where('flights.origin_id = ?', id).order(start: :asc)
-    elsif params.keys.include?('to')
-      id = find_airport_id params['to']
-      flights = Flight.where('flights.destination_id = ?', id).order(start: :asc)
-    else
-      flights = Flight.all.order(start: :asc)
+      flights = flights.where('flights.destination_id = ?', destination_id)
     end
 
     if params.keys.include?('date')
-      puts " HI YOOOOOOOO"
-      puts "flights so far.. #{flights.length}"
       formatted_date = format_date(params['date'])
-      puts "date: #{formatted_date}"
-
       flights = flights.where("DATE(start) = ?", formatted_date)
     end
 
