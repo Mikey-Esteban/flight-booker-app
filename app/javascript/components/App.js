8cad0b79fb10874f1162f9e0de970333beeabf61
Mikey Esteban <michaelp.esteban@gmail.com>
2021-02-09 09:49:57 -0500

    moved Checkout and Passenger to its own folder

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Route, Switch } from 'react-router-dom'
 import Flights from './Flights/Flights'
-import Checkout from './Flights/Checkout'
+import Checkout from './Checkout/Checkout'
 
 const App = () => {
   return (
